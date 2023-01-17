960e1185c12ee51a5808c3fcb71e648ba432a766
Mike <kaynov.m@gmail.com>
2023-01-18 00:31:12 +0300

    done

--- main.py
+++ main.py
@@ -2,6 +2,20 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium_stealth import stealth
 import time
+import databases
+from sqlalchemy import create_engine
+from typing import List
+from sqlalchemy import MetaData, Table, String, Integer, Column, Text, DateTime, Boolean, ForeignKey, create_engine, \
+    func, select, Date, desc, literal_column, insert
+from datetime import datetime
+from models import metadata, comps
+import psycopg2
+
+
+engine = create_engine('postgresql://postgres:s1t@localhost/test_1')
+conn = engine.connect()
+ins = insert(comps)
+
 
 options = webdriver.ChromeOptions()
 options.add_argument("start-maximized")
@@ -21,33 +35,69 @@ stealth(driver,
         fix_hairline=True,
         )
 
+data = {}
+# counts = 0
 
-for page in range(2):
+
+for page in range(1, 20):
         url = f"https://www.holodilnik.ru/digital_tech/notebook/sankt-peterburg/?page={page}"
         driver.get(url)
-        time.sleep(3)
+        time.sleep(1)
 
         blocks = driver.find_elements(By.CLASS_NAME, "product-specification")
 
         for block in blocks:
                 item_name = block.find_element(By.CLASS_NAME, "product-name").find_element(By.TAG_NAME, "a").find_element(By.TAG_NAME, "span").text
-                print(item_name)
-                links = block.find_element(By.CLASS_NAME, "product-name").find_element(By.TAG_NAME, "a").get_attribute("href")
-                print(links)
+                link = block.find_element(By.CLASS_NAME, "product-name").find_element(By.TAG_NAME, "a").get_attribute("href")
                 price = block.find_element(By.CLASS_NAME, "item-order").find_element(By.CLASS_NAME, "price").text.replace(' ', '').replace('₽', '')
-                print(price)
+                if '\n' in price:
+                        price = price.split('\n')[1]
+                data[item_name] = {'item_name': item_name,
+                        'items_link': link,
+                        'price': price
+                }
 
 
-        # block_orders = driver.find_elements(By.CLASS_NAME, "item-order").find_element(By.CLASS_NAME, "price")
-        # print(block_orders)
+for link in data:
+        driver.get(data[link]['items_link'])
+        time.sleep(1)
 
-                # for block_order in block_orders:
-                #         price = block_order.find_element(By.TAG_NAME, "a").get_attribute("href")
+        cpu = driver.find_element(By.CSS_SELECTOR,
+                                  '#opisAndTTH > div.det-content-block > div.det-content-inner > div > div:nth-child(7) > div.params-list__item-value > span').text
+        try:
+                ram = float(driver.find_element(By.CSS_SELECTOR,
+                                      '#opisAndTTH > div.det-content-block > div.det-content-inner > div > div:nth-child(12) > div.params-list__item-value > span').text)
+        except:
+                print(data[link]['items_link'])
+                print(data[link]['price'])
+                print(ram)
+                ram = 8
 
-        # for post in posts:
-        #         title = post.find_element(By.CLASS_NAME, )
+        rate = 0
+        try:
+                if cpu[0:3] == 'Int':
+                        rate = (150 + ram * 200 + (float(data[link]['price']) * -0.005))
+                elif cpu[0:3] == 'AMD':
+                        rate = (100 + ram * 200 + (float(data[link]['price']) * -0.005))
+                else:
+                        rate = (50 + ram * 200 + (float(data[link]['price']) * -0.005))
+        except:
+                continue
 
-# url = "https://www.holodilnik.ru/"
-# driver.get(url)
-# time.sleep(5)
-# driver.quit()
+#         print(data[link]['item_name'])
+#         print(float(data[link]['price']))
+#         print(data[link]['items_link'])
+# #        print(rate)
+#         counts += 1
+#         print(counts)
+#         print()
+        r = conn.execute(ins,
+                         items_name=data[link]['item_name'],
+                         price=float(data[link]['price']),
+                         items_link=data[link]['items_link'],
+                         rate=rate
+                         )
+
+
+
+
--- models.py
+++ models.py
@@ -1,7 +1,9 @@
 import databases
 from sqlalchemy import MetaData, Table, String, Integer, Column, Text, DateTime, Boolean, ForeignKey, create_engine, \
-    func, select, Date
+    func, select, Date, insert, Float
 from datetime import datetime, date
+import psycopg2
+
 
 metadata = MetaData()
 
@@ -10,7 +12,23 @@ comps = Table(
     metadata,
     Column("id", Integer, primary_key=True),
     Column("items_name", String, index=True),
-    Column("price", Integer),
-    Column("rate", Integer),
+    Column("items_link", String, index=True),
+    Column("price", Float),
+    Column("rate", Float),
     Column("create_date", Date, default=datetime.now())
 )
+
+
+engine = create_engine('postgresql://postgres:s1t@localhost/test_1')
+metadata.create_all(engine)
+
+
+conn = engine.connect()
+ins = insert(comps)
+
+# r = conn.execute(ins,
+#     items_name="test2",
+#     price=123,
+#     rate=1
+#     )
+
